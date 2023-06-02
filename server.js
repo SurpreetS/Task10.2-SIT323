306dd0e5b3a11449458a7aaac6f0727ef9ab9a80
Surpreet Singh <surpreetsingh@deakin.edu.au>
2023-06-03 00:57:45 +1000

    updated

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Task 10.2 \n\n\nHi! i am surpreet singh. I am a final year student of bachelor of IT(majoring in cloud computing) at Deakin University.I am doing SIT323 which is one of my major units and I am aiming for HD.');
+  res.send('Hello there');
 });
 
 app.listen(PORT, HOST, () => {
