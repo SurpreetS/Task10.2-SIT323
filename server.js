28cff58ab49fe2391e1710f2fa39626d9275ab99
Surpreet Singh <surpreetsingh@deakin.edu.au>
2023-06-02 23:28:07 +1000

    updated

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('My Final Changes now');
+  res.send('My Final Changes now1');
 });
 
 app.listen(PORT, HOST, () => {
